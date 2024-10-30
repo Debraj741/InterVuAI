5d19fb6dcdfee76e6f4442f77cbff70475ab742f
Debraj Kundu <debrajkundu657@gmail.com>
2024-10-30 12:51:08 +0530

    LAyout Changes

---
+++ app/dashboard/page.jsx
@@ -0,0 +1,12 @@
+import { UserButton } from '@clerk/nextjs'
+import React from 'react'
+
+export default function Dashboard() {
+  return (
+    <div>
+      Hello Dashboard
+      <UserButton/>
+    </div>
+  )
+}
+
